01ca55ac2e886a45fc7a4e398c03714549738702
Jessica <1330784336@qq.com>
2022-04-26 17:02:03 +0800

    type模块js方法

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <router-view />
-    <span>111</span>
   </div>
 </template>
 
--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li
+        :class="type === '-' && 'selected'"
+        @click="selectedType((type = '-'))"
+      >
+        支出
+      </li>
+      <li
+        :class="type === '+' && 'selected'"
+        @click="selectedType((type = '+'))"
+      >
+        收入
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      type: "-",
+    };
+  },
+  methods: {
+    selectedType(type) {
+      if (type !== "-" && type !== "+") {
+        throw new Error("type is unknown");
+      }
+      this.type = type;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
